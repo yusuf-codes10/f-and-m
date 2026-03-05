f517eb0d35ecb715782f12c9be759ce2291fc9eb
Yusef Codes <youcefcopy@gmail.com>
2026-03-05 11:17:15 +0100

    cartItems array

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,9 +1,19 @@
 // pinia store for the cart system
 import { defineStore } from "pinia";
+import {ref} from 'vue'
 
 
 export const cartStore = defineStore('cart', () => {
+    // * state
+    const cartItems = ref([])
 
+
+
+
+
+    return{
+        cartItems,
+    }
 })
 
 // TODO: the cart logic such as add/increase quantity
