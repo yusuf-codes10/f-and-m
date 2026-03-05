abd5ad96900992d4ecf33f5ad8f19cdfb11db9ab
Yusef Codes <youcefcopy@gmail.com>
2026-03-05 12:25:15 +0100

    trying to render the product form the cart array in the store

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,14 +1,23 @@
 <script setup>
-        
+    import { cartStore } from '@/stores/cartStore'
+import FProduct from '@/components/FProduct.vue'
+
+
+    const myCartStore = cartStore()
+    console.log(myCartStore)
 </script>
 <template>
     <div class="cart-page">
-        Your Cart
+    This is the cart 
+    <h3>The products in cart</h3>
+    <div class="products">
+      <FProduct v-for="item in myCartStore.cartItems" :key="item.product.id" :product="item.product" />
     </div>
+  </div>
 </template>
 <style scoped>
-    .cart-page {
-        height: 100dvh;
-        background-color: sienna;
-    }
+.cart-page {
+  height: 100dvh;
+  background-color: sienna;
+}
 </style>
