67c4ac8a0c0feb8001674da1c781c803da328752
Yusef Codes <youcefcopy@gmail.com>
2026-03-06 10:49:59 +0100

    styling the images

--- src/components/cart/CartProduct.vue
+++ src/components/cart/CartProduct.vue
@@ -14,14 +14,16 @@ const currentColor = computed(() =>
   props.item.product.colors.find((c) => c.name === selectedColor.value)
 )
 
-console.log(currentColor);
+console.log(currentColor)
 </script>
 <template>
-  <div class="item" >
-    <h1>{{ props.item.product.title }}</h1>
-    <p>{{ props.item.quantity }}</p>
+  <div class="item">
     <div class="image">
-        <img :src="currentColor?.images[0]" :alt="props.item.product.title">
+      <img :src="currentColor?.images[0]" :alt="props.item.product.title" />
+    </div>
+    <div class="content">
+      <h1>{{ props.item.product.title }}</h1>
+      <p>{{ props.item.quantity }}</p>
     </div>
   </div>
 </template>
@@ -32,5 +34,12 @@ console.log(currentColor);
   height: 100px;
   width: 80%;
   margin: 1rem 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.image img {
+  width: 100%;
+  height: 100%;
 }
 </style>
