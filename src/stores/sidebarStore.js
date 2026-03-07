c86b186e98d918ea1100a17d1d35509ad3bc9464
Yusef Codes <youcefcopy@gmail.com>
2026-03-07 11:36:01 +0100

    isOpen state defined in the pinia store

--- src/stores/sidebarStore.js
+++ src/stores/sidebarStore.js
@@ -1,4 +1,14 @@
 // ! pinia store for the sidebar and the mobile menu
 import { defineStore } from 'pinia'
+import {ref} from 'vue'
 
-export const cartStore = defineStore('sidebar', () => {})
+export const cartStore = defineStore('sidebar', () => {
+
+    const isOpen = ref(false)
+
+
+
+    return {
+        isOpen,
+    }
+})
