2d30d51654f43339798234b7cf0a0e68e6d7db15
Yusef Codes <youcefcopy@gmail.com>
2026-03-07 10:09:00 +0100

    men shoes placeholder

--- src/data/men-shoes.js
+++ src/data/men-shoes.js
@@ -1,1 +1,37 @@
 // men shoes array is going to be here
+const menShoes = [
+  {
+    id: 1,
+    title: 'Air Jordan 1 mid',
+    description: 'This iteration of the AJ1 reimagines Mike\'s first signature model with a fresh mix of colors. Premium materials, soft cushioning and a padded ankle collar offer total support and celebrate the shoe that started it all.',
+    defaultColor: 'blue',
+    for: 'Mem',
+    status: 'BestSeller',
+    price: 75,
+    isFavorite: false,
+    slug: 'air-jordan-1-mid',
+    category: 'men-shoes',
+    stock: 5,
+
+    colors: [
+      {
+        name: 'black',
+        images: [
+          '/images/air-jordan-1-mid.avif',
+          '/images/air-jordan-1-mid-profile.avif',
+          '/images/air-jordan-1-mid-back.avif',
+          '/images/air-jordan-1-mid-top.avif',
+        ],
+      },
+      {
+        name: 'blue',
+        images: [
+          '/images/air-jordan-1-mid-blue.avif',
+          '/images/air-jordan-1-mid-blue-profile.avif',
+          '/images/air-jordan-1-mid-blue-back.avif',
+          '/images/air-jordan-1-mid-blue-top.avif',
+        ],
+      },
+    ],
+  },
+]
--- src/views/menView.vue
+++ src/views/menView.vue
@@ -1,10 +1,41 @@
+<script setup>
+// import FProduct from './FProduct.vue'
+// import shoes from '@/data/women-shoes.js'
+import FProduct from '@/components/FProduct.vue';
+import { productsStore } from '@/stores/productStore'
+
+// we have to set the store
+const myStore = productsStore()
+console.log(myStore.shoes)
+
+// import { ref, onMounted } from 'vue'
+
+// fetch data from an api
+// const array = ref([])
+// onMounted(async () => {
+//   try {
+//     const response = await fetch('https://fakestoreapi.com/products')
+//     const data = await response.json()
+//     array.value = data
+
+//     console.log(array.value)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
+
+// search by name feature
+</script>
+
 <template>
-  <div class="men">This is the men products</div>
+  <div class="products women">
+    <FProduct v-for="shoe in myStore.activeFilter" :key="shoe.id" :product="shoe" />
+  </div>
 </template>
 
 <style scoped>
-.men {
+.women {
   min-height: 100dvh;
-  background-color: sienna;
+  /* background-color: seagreen; */
 }
 </style>
