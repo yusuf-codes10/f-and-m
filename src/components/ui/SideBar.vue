9e4cf79dfbb385cb5f9e8dec03e4904f54e5977f
Yusef Codes <youcefcopy@gmail.com>
2026-03-23 19:00:12 +0100

    sidebar completed and styled

--- src/components/ui/SideBar.vue
+++ src/components/ui/SideBar.vue
@@ -1,14 +1,11 @@
 <script setup>
+import { defineProps } from 'vue'
 
-import { defineProps } from 'vue';
+defineProps({
+  open: Boolean,
+})
 
-defineProps(
-    {
-        open: Boolean,
-    }
-)
-
-import {sideBarStore} from '@/stores/sidebarStore.js'
+import { sideBarStore } from '@/stores/sidebarStore.js'
 
 const myStore = sideBarStore()
 
@@ -18,10 +15,15 @@ const close = () => {
 </script>
 
 <template>
-    <div class="sidebar"  :class="{ open: myStore.isOpen }">
-        this is the side bar
-        <i @click="close" class="fa-solid fa-xmark"></i>
-    </div>
+  <div class="sidebar" :class="{ open: myStore.isOpen }">
+    <i @click="close" class="fa-solid fa-xmark"></i>
+    <ul>
+      <li><RouterLink :to="{ name: 'Women' }"> Women</RouterLink></li>
+      <li><RouterLink :to="{ name: 'Men' }">Men</RouterLink></li>
+      <li><RouterLink :to="{ name: 'Bags' }">Bags & Accesories</RouterLink></li>
+      <li><RouterLink :to="{ name: 'Favorites' }">Favorites</RouterLink></li>
+    </ul>
+  </div>
 </template>
 <style scoped>
 /* transform & transition instead of the display:none for better animation later */
@@ -38,9 +40,36 @@ const close = () => {
   transform: translateX(100%); /* push it to the right */
   transition: transform 0.3s ease;
   z-index: 40;
+
+  padding: 25px 2rem;
+  border-left: 1px solid var(--text-color);
+  /* position: relative; */
 }
 
 .sidebar.open {
   transform: translateX(0);
+}
+
+.sidebar i {
+  position: absolute;
+  top: 25px;
+  right: 10px;
+
+  cursor: pointer;
+}
+
+.sidebar ul {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  list-style-type: none;
+}
+
+ul li {
+  text-decoration: underline;
+}
+ a {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
